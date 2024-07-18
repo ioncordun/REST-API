eaa16bf0bf40613c9fd208f1a9a7bb5c133dd908
ioncordun <ioncordun@gmail.com>
2024-07-18 16:46:38 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -156,9 +156,8 @@ export default {
         console.log(data)
         this.operatore = data.operatore
         this.sito_operatore = data.sito_operatore
-        this.id_stazione = data.id_stazione
         
-        this.stations.id_stazione = data.stations.id_stazione
+        this.stations[0].id_stazione = data.stations[0].id_stazione
         this.stations[0].nome_stazione = data.stations[0].nome_stazione
         this.stations[0].cap = data.stations[0].cap
         this.stations[0].lat = data.stations[0].lat
